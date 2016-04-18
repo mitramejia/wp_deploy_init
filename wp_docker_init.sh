3d8cbdad3871b29eee5b30cac8f840bec68f8a87
Mitra Mejia <mitra.mejia@gmail.com>
2016-04-18 12:55:00 -0400

    Improve dir management

--- wp_docker_init.sh
+++ wp_docker_init.sh
@@ -2,42 +2,46 @@
 
 set -o errexit
 set -o xtrace
+set -o errtrace
 set -o nounset
 
 #
-# ====================================================================================
+# ============================================================================
 # VARIABLES
-# ====================================================================================
+# ============================================================================
 #
 
-random_pass="$(openssl rand -base64 32)"
-app_dir=~/app
-script_dir=~/wp_docker_init/
-droplet_ip=$(wget http://ipinfo.io/ip -qO -);
+# Set magic variables for current file and its directory.
+# BASH_SOURCE[0] is used so we can display the current file even if it is sourced by a parent script.
+# If you need the script that was executed, consider using $0 instead.
+
+# random_pass="$(openssl rand -base64 32)"
+# droplet_ip=$(wget http://ipinfo.io/ip -qO -);
+
+__script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__app_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ./ && pwd)"/app
+__file="${__script_dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+
 
 # ------------------------------------------------
 # Colors
 # ------------------------------------------------
 
 red='\033[0;31m'     
-light_red='\033[1;31m'
-green='\033[0;32m'     
-light_green='\033[1;32m'
+green='\033[1;32m'     
 yellow='\033[1;33m'
-light_blue='\033[1;34m'
+blue='\033[1;34m'
 purple='\033[0;35m'     
-cyan='\033[1;36m'     
-light_cyan='\033[1;36m'
-light_gray='\033[0;37m'     
-white='\033[1;37m'
+gray='\033[1;36m'         
+white='\033[0;37m'
 no_color='\033[0m'
 
 
-
 #
-# ====================================================================================
+# ============================================================================
 # FUNCTIONS
-# ====================================================================================
+# ============================================================================
 #
 
 # ------------------------------------------------
@@ -52,21 +56,20 @@ function prompt() {
 
 
 #
-# ====================================================================================
+# =============================================================================
 # BODY
-# ====================================================================================
+# =============================================================================
 #
+
 
 # ------------------------------------------------
 # Make sure the script is being ran as root
 # ------------------------------------------------
 
 if [[ $EUID -ne 0 ]]; then
-   prompt $cyan "This script must be run as root"
+   prompt ${purple} "This script must be run as root"
    exit 1
 fi
-
-prompt $purple "Configuring git..."
 
 
 # ------------------------------------------------
@@ -74,21 +77,22 @@ prompt $purple "Configuring git..."
 # ------------------------------------------------
 
 if hash ruby 2>/dev/null; then 
-	prompt $cyan "Ruby is already installed."
+	prompt ${purple} "Ruby is already installed"
 else
-	prompt $green "Configuring git..."
+	prompt ${purple} "Installing ruby..."
 	apt-get install ruby
 	gem install colorize
 fi
+
 
 # ------------------------------------------------
 # Download and install git
 # ------------------------------------------------
 # 
 if hash git 2>/dev/null; then 
-	prompt $cyan "SCM_Breeze is already installed."
+	prompt ${purple} "SCM_Breeze is already installed"
 else
-	prompt $green "Installing SCM_Breeze..."
+	prompt ${purple} "Installing SCM_Breeze..."
 	git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze
 	~/.scm_breeze/install.sh
 	source ~/.bashrc   # or source ~/.zshrc
@@ -99,30 +103,32 @@ fi
 # Download and install python pip
 # ------------------------------------------------
 
-# Check if pip is installed
 if hash pip 2>/dev/null; then 
-	prompt $cyan "Python Pip is already installed."
+	prompt ${purple} "Python Pip is already installed."
 else
-	prompt $green "Installing Pyhton Pip..."
+	prompt ${purple} "Installing Pyhton Pip..."
 	apt-get -y install python-pip;
 fi
+
 
 # ------------------------------------------------
 # Download and install docker compose
 # ------------------------------------------------
 
 if hash docker-compose 2>/dev/null; then 
-	prompt $cyan "Docker Compose is already installed."
+	prompt ${purple} "Docker Compose is already installed."
 else
-	prompt $green "Installing Docker Compose..."
+	prompt ${purple} "Installing Docker Compose..."
 	pip install docker-compose
 fi
+
 
 # ------------------------------------------------
 # Create app directory
 # ------------------------------------------------
 
-mkdir $app_dir && cd $app_dir
+mkdir ${__app_dir} && cd ${__app_dir}
+
 
 # ------------------------------------------------
 # Create docker-compose.yml
@@ -150,47 +156,51 @@ phpmyadmin:
   environment:
     MYSQL_USERNAME: root
     MYSQL_ROOT_PASSWORD: ${random_pass}
-    " > $app_dir/docker-compose.yml 
+    " > ${__app_dir}/docker-compose.yml 
 
 
 # ------------------------------------------------
 # Download and install docker images
-# ------------------------------------------------
-# 
-prompt $green "Pulling docker images..."
+# ------------------------------------------------ 
+
+prompt ${purple} "Pulling docker images..."
 
 docker pull ubuntu
 docker pull mariadb
 docker pull corbinu/docker-phpmyadmin	
 
+
 # ------------------------------------------------
 # Start Docker containers
 # ------------------------------------------------
-prompt $green "Starting docker containers..."
+
+prompt ${purple} "Starting docker containers..."
 cd $app_dir
 docker-compose up -d
+
 
 # ------------------------------------------------
 # Configure git
 # ------------------------------------------------
 
-prompt $green "Configuring git..."
-rm -rf $(find ~/ -name "wp-content")
-mkdir -p $app_dir/wp-content  && cd $app_dir/wp-content
+prompt ${purple} "Configuring git..."
+rm -rf $(find ${__app_dir} -name "wp-content")
+mkdir -p ${__app_dir}/wp-content  && cd ${__app_dir}/wp-content
 mkdir production.git && cd production.git
 git init --bare
-cp $script_dir/post-receive $app_dir/wp-content/production.git/hooks/
+cp ${__script_dir}/post-receive ${__app_dir}/wp-content/production.git/hooks/
 chmod -x post-receive
+
 
 # ------------------------------------------------
 # Display instructions deploy from local machine
 # ------------------------------------------------
 
-prompt $light_blue "Now go to your repo and run these commands to setup your local repo to deploy here"
-prompt $white "git remote add production ssh://${droplet_ip}}/~/app/wp-content/production.git"
-prompt $white "git checkout master"
-prompt $white "git push origin master"
-prompt $green "Done"
+prompt ${purple} "Now go to your repo and run these commands to setup your local repo to deploy here"
+prompt ${white} "git remote add production ssh://${droplet_ip}}/~/app/wp-content/production.git"
+prompt ${white} "git checkout master"
+prompt ${white} "git push origin master"
+prompt ${green} "Done"
 
 
 
